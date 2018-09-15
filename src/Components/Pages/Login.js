efe61c2e39f8e9024d23c77dcf95126ba37cd600
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-09-15 15:23:03 -0300

    avance 3

--- src/Components/Pages/Login.js
+++ src/Components/Pages/Login.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 //  import UserForm from '../Login/Userform'
 import PrintList from '../Login/PrintList'
+import Logo4u from './../Logo/Logo' // ruta
 
 class Login extends Component {
   render() {
     return (
       <div className="App">
        
-       
+       <Logo4u />
         <PrintList />
       </div>
 
